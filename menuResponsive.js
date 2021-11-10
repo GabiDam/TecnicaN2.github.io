4b684a221e3cd408ffe683e6005da1e717f96669
Fidel <92132155+FidelBarreiros@users.noreply.github.com>
2021-11-10 15:17:05 -0300

    sambullido

--- menuResponsive.js
+++ menuResponsive.js
@@ -20,20 +20,59 @@ jQuery('document'). ready(function($){
     });
     
 
-    var boton = $('info1'),
-    menu = $('.navigation ul');
+    var boton = $('.info1'),
+        carlo = $('.imgs a');
 
-boton.click(function(){
+    boton.click(function(){
 
-    if(menu.hasClass('show')){
+        if(carlo.hasClass('show2')){
 
-        menu.removeClass('show');
+            return
         
-    }
-    else{
+        }
+        else{
 
-        menu.addClass('show');
+            carlo.addClass('show2');
+            porfa.removeClass('txth');
+            porfa.addClass('bgo');
+        }
+
+
+    });
+
+
+    var boton2 = $('.info2'),
+    carlo2 = $('.imgs ul');
+    porfa = $('.txth')
+
+     boton2.click(function(){
+
+        if(carlo2.hasClass('show3')){
+    
+            return
         
+        }
+        else{
+    
+            carlo2.addClass('show3');
+            porfa.removeClass('txth2');
+            porfa.addClass('bgo2');
+        }
+    
+    
+    });
+    
+
+    var botonc1 = $('.cerrar'),
+    carlo = $('.imgs a');
+
+    botonc1.click(function(){
+
+    if(carlo.hasClass('show2')){
+
+        carlo.removeClass('show2');
+        porfa.removeClass('bgo');
+        porfa.addClass('txth')
     }
 
 
